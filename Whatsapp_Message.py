b5c67d6e0a652657a499d01e12f652b3499087e5
Vinotha27 <108472665+Vinotha27@users.noreply.github.com>
2023-03-20 22:08:55 +0530

    Update Whatsapp_Message.py

--- Whatsapp_Message.py
+++ Whatsapp_Message.py
@@ -1,5 +1,5 @@
 import streamlit as st
-#!pip install pywhatkit
+!pip install pywhatkit
 import pywhatkit
 wm=pywhatkit.sendwhatmsg("+917010683112","This is a automate whatsapp Message,Your pending payment of Rs.17,000,If you already paid the due ignore this message",11,8)
 st.write(wm)
